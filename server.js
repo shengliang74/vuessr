8188c732c7970abbd45baa1b2469b73f63c36baa
shengliang74 <shengliang74@yeah.net>
2018-09-26 12:25:05 +0800

    server

--- server.js
+++ server.js
@@ -4,17 +4,35 @@ const webpack = require('webpack');
 const fs = require('fs');
 const MFS = require('memory-fs');
 const path = require('path');
+const resolve = file => path.resolve(__dirname, file);
 const webpackDevMiddleware = require('webpack-dev-middleware');
 // const renderer = require('vue-server-renderer').createRenderer();
 const { createBundleRenderer } = require('vue-server-renderer')
 // const template = require('fs').readFileSync('./index.template.html', 'utf-8');
 const template = require('fs').readFileSync('./index.template.html', 'utf-8')
+const templatePath = resolve('./index.template.html')
 
 const app = express();
 const isProd = process.env.NODE_ENV === 'production';
+
+let renderer = null;
 let readyPromise = null;
 
 
+function createRenderer (bundle, options) {
+  // https://github.com/vuejs/vue/blob/dev/packages/vue-server-renderer/README.md#why-use-bundlerenderer
+  return createBundleRenderer(bundle, Object.assign(options, {
+    // for component caching
+    cache: LRU({
+      max: 1000,
+      maxAge: 1000 * 60 * 15
+    }),
+    // this is only needed when vue-server-renderer is npm-linked
+    basedir: resolve('./dist'),
+    // recommended for performance
+    runInNewContext: false
+  }))
+}
 
 if(isProd){
   // const createApp = require('/path/to/built-server-bundle.js')
@@ -26,17 +44,17 @@ if(isProd){
   })
 
 }else{
-  const fs = require('fs');
-  const MFS = require('memory-fs');
-  const clientConfig = require('./build/webpack.client.config.js');
-  const serverConfig = require('./build/webpack.server.config.js');
+  // const fs = require('fs');
+  // const MFS = require('memory-fs');
+  // const clientConfig = require('./build/webpack.client.config.js');
+  // const serverConfig = require('./build/webpack.server.config.js');
 
-  const readFile = (fs, file) => {
-    try {
-      console.log(clientConfig.output.path,"clientConfig.output.path");
-      return fs.readFileSync(path.join(clientConfig.output.path, file), 'utf-8')
-    } catch (e) {}
-  }
+  // const readFile = (fs, file) => {
+  //   try {
+  //     console.log(clientConfig.output.path,"clientConfig.output.path");
+  //     return fs.readFileSync(path.join(clientConfig.output.path, file), 'utf-8')
+  //   } catch (e) {}
+  // }
 
   //客户端配置
   // const compiler = webpack(config);
@@ -45,39 +63,50 @@ if(isProd){
   // }));
 
   //服务端配置
-  const serverCompiler = webpack(serverConfig)
-  const mfs = new MFS()
-  let bundle = null;
+  // const serverCompiler = webpack(serverConfig)
+  // const mfs = new MFS()
+  // let bundle = null;
 
-  const update = ()=>{
-    if(bundle){
-      renderer = createBundleRenderer(bundle, {
-        template, // （可选）页面模板
-        // clientManifest // （可选）客户端构建 manifest
-      })
+  // const update = ()=>{
+  //   if(bundle){
+  //     renderer = createBundleRenderer(bundle, {
+  //       template, // （可选）页面模板
+  //       // clientManifest // （可选）客户端构建 manifest
+  //     })
+  //   }
+  // }
+
+  // serverCompiler.outputFileSystem = mfs;
+  // serverCompiler.watch({}, (err, stats) => {
+  //   console.log("11111111111111");
+  //   if(err) throw err;
+  //   console.log("2222222222222");
+  //   stats = stats.toJson();
+  //   console.log("3333333333333333");
+  //   console.log(stats.errors);
+  //   if(stats.errors.length) return
+  //   console.log("44444444444444444444");
+  //   bundle = JSON.parse(readFile(mfs, 'vue-ssr-server-bundle.json'));
+  //   console.log("bundlebundlebundle");
+  //   update();
+  // })
+
+  readyPromise = require('./build/setup-dev-server')(
+    app,
+    templatePath,
+    (bundle, options) => {
+      renderer = createRenderer(bundle, options)
     }
-  }
-
-  serverCompiler.outputFileSystem = mfs;
-  serverCompiler.watch({}, (err, stats) => {
-    console.log("11111111111111");
-    if(err) throw err;
-    console.log("2222222222222");
-    stats = stats.toJson();
-    console.log("3333333333333333");
-    console.log(stats.errors);
-    if(stats.errors.length) return
-    console.log("44444444444444444444");
-    bundle = JSON.parse(readFile(mfs, 'vue-ssr-server-bundle.json'));
-    console.log("bundlebundlebundle");
-    update();
-  })
+  )
 }
 
 app.get('*', (req, res) => {
-	const context = { url: req.url }
+	const context = { url: req.url };
+  console.log("1234",req.url);
+  console.log("1234",readyPromise);
   // createApp(context).then(app => {
     readyPromise.then((req, res)=>{
+      console.log("renderer",renderer);
       renderer.renderToString(context, (err, html) => {
         if (err) {
           if (err.code === 404) {
